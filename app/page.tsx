195170d204c949092bff246a60cb07f0bb0fda94
Dan Davis <dan@thedandavis.com>
2025-06-28 19:11:26 -0700

    The One link

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,8 @@ import React from "react";
 import Particles from "./components/particles";
 
 const navigation = [
-  { name: "Projects", href: "/projects" },
-  { name: "Contact", href: "/contact" },
-];
+  { name: "The One", href: "https://thedandavis.com" },
+  ];
 
 export default function Home() {
   return (
@@ -35,14 +34,7 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          Today's Solutions{" "}
-          <Link
-            target="_blank"
-            href="https://unkey.dev"
-            className="underline duration-500 hover:text-zinc-300"
-          >
-           To Saving
-          </Link> humanity, our world, and the universe as we know ikt.
+          The equation to life forever coming soon.
         </h2>
       </div>
     </div>
