87ff98b5178cf7182b93fa580fbf71bbddfc498c
Daniel (Dan) Roy Davis <dan@thedandavis.com>
2026-01-12 00:39:54 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          The equation to life equals home.one, everything has only one home. Stories of these lessons and more coming soon.
+Respecting we all only have one home leads to forever life
         </h2>
       </div>
     </div>
